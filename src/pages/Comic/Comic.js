2e2ce1bd13347b10082c52b13d55c7e7f547c6dd
Dmytro <dimaniko04@ukr.net>
2021-06-10 23:12:51 +0300

    Now Comic.js not only renders the title of the comic, but also a comic itself.Added a button to switch to another random comic.

--- src/pages/Comic/Comic.js
+++ src/pages/Comic/Comic.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Container, Content, H1, Spinner } from "native-base";
+import { Image, Dimensions } from "react-native";
+import {
+  Button,
+  Container,
+  Content,
+  H1,
+  Spinner,
+  Text,
+  View,
+} from "native-base";
 import { DefaultHeader } from "../../components/DefaultHeader";
 
 const LAST_COMIC = 2473;
 
-function getRandomInt() {
+function getRandomComic() {
   return Math.floor(Math.random() * LAST_COMIC) + 1;
 }
 
 export const Comic = () => {
-  const [comicNumber, setComicNumber] = useState(getRandomInt());
+  const [comicNumber, setComicNumber] = useState(getRandomComic());
   const [comicInfo, setComicInfo] = useState();
 
   useEffect(() => {
@@ -19,6 +28,10 @@ export const Comic = () => {
           `https://xkcd.com/${comicNumber}/info.0.json`
         );
         const json = await response.json();
+        await Image.getSize(json.img, (width, height) => {
+          json.width = width;
+          json.height = height;
+        });
 
         setComicInfo(json);
       } catch (err) {
@@ -29,11 +42,39 @@ export const Comic = () => {
     getData();
   }, [comicNumber]);
 
+  if (!comicInfo)
+    return (
+      <Container>
+        <DefaultHeader />
+        <Spinner />
+      </Container>
+    );
+
+  const width = Math.min(comicInfo.width, Dimensions.get("window").width - 20);
+  const height = width * (comicInfo.height / comicInfo.width);
+
   return (
     <Container>
       <DefaultHeader title="Comic" />
-      <Content>
-        {comicInfo === undefined ? <Spinner /> : <H1>{comicInfo.title}</H1>}
+      <Content
+        padder
+        contentContainerStyle={{
+          alignItems: "center",
+        }}
+      >
+        <H1>{comicInfo.title}</H1>
+        <View style={{ margin: 10 }}>
+          <Button onPress={() => setComicNumber(getRandomComic())}>
+            <Text>Random</Text>
+          </Button>
+        </View>
+        <Image
+          source={{ uri: comicInfo.img }}
+          style={{
+            width: width,
+            height: height,
+          }}
+        />
       </Content>
     </Container>
   );
