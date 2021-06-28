4e900655aeedec54f0dd7475a240802b5a68223e
Dmytro <dimaniko04@ukr.net>
2021-06-28 11:53:33 +0300

    Changed app icon.Custom splash screen was added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Cryptocurrencies } from "./pages/Cryptocurrencies/Cryptocurrencies.js";
 import { TodoList } from "./pages/TodoList/TodoList.js";
 import { Comic } from "./pages/Comic/Comic.js";
 import { Weather } from "./pages/Weather/Weather.js";
+import RNBootSplash from "react-native-bootsplash";
 
 const Drawer = createDrawerNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={() => RNBootSplash.hide()}>
       <Drawer.Navigator>
         <Drawer.Screen name="Cryptocurrencies" component={Cryptocurrencies} />
         <Drawer.Screen name="TodoList" component={TodoList} />
